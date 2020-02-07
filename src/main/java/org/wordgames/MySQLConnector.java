12b6622d15b604a93325dcc8149acc9b5ec22a79
Daniel <golikov.danya@gmail.com>
2020-02-07 21:11:32 +0200

    hhh

--- src/main/java/org/wordgames/MySQLConnector.java
+++ src/main/java/org/wordgames/MySQLConnector.java
@@ -4,7 +4,7 @@ import java.sql.*;
 import java.util.*;
 
 class MySQLConnector {
-    private String url = "jdbc:mysql://localhost/mysql";
+    private String url = "jdbc:mysql://localhost/mysql?serverTimezone=Europe/Moscow&useSSL=false";
     private String username = "daniel";
     private String password = "password";
 
@@ -12,6 +12,9 @@ class MySQLConnector {
         try {
             Connection con;
             con = DriverManager.getConnection(url, username, password);
+
+            String url = "jdbc:mysql://localhost/store?serverTimezone=Europe/Moscow&useSSL=false";
+
             PreparedStatement ps = con.prepareStatement("SELECT wn_pro_mysql.wn_synset.word, wn_pro_mysql.wn_gloss.gloss\n" +
                     "FROM wn_pro_mysql.wn_synset\n" +
                     "INNER JOIN wn_pro_mysql.wn_gloss ON wn_pro_mysql.wn_synset.synset_id = wn_pro_mysql.wn_gloss.synset_id\n" +
