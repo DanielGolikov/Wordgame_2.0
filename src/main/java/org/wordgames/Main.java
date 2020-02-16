4755fff6a21bc175cc8a784bab6f509feb99fab9
daniel <golikov.danya@gmail.com>
2020-02-16 19:59:08 +0200

    time-recording logic

--- src/main/java/org/wordgames/Main.java
+++ src/main/java/org/wordgames/Main.java
@@ -10,7 +10,12 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+// поиск смысла жизни ...
         ArrayList words = getAllPossibleValues("");
         MySQLWordChecker.findAllWordsFromArray(words);
+        long finish = System.currentTimeMillis();
+        long timeConsumedMillis = finish - start;
+        System.out.println(timeConsumedMillis);
     }
 }
