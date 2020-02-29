065fdb0079e444ebf22b492597f5cbdadafe70cc
daniel <golikov.danya@gmail.com>
2020-02-29 23:46:25 +0200

    0

--- src/main/java/org/wordgames/Main.java
+++ src/main/java/org/wordgames/Main.java
@@ -12,7 +12,6 @@ public class Main {
         long start = System.currentTimeMillis();
         MySQLWordChecker.findAllWordsFromArray(words);
         long finish = System.currentTimeMillis();
-
         long timeConsumedMillis = finish - start;
         System.out.println(timeConsumedMillis);
         start = System.currentTimeMillis();
