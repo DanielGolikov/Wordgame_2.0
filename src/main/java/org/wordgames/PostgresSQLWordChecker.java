ddf9c3674a3cb840bd1acef58eba3749d17ce946
daniel <golikov.danya@gmail.com>
2020-03-05 23:41:41 +0200

    csv inserting

--- src/main/java/org/wordgames/PostgresSQLWordChecker.java
+++ src/main/java/org/wordgames/PostgresSQLWordChecker.java
@@ -1,5 +1,9 @@
 package org.wordgames;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -18,14 +22,25 @@ public class PostgresSQLWordChecker {
             PreparedStatement ps = con.prepareStatement("create temporary table temporary_results(word varchar(15) primary key);");
             ps.executeUpdate();
             System.out.println(allPossibleValues.size());
-            ps = con.prepareStatement("INSERT INTO temporary_results VALUES (?)");
+//            ps = con.prepareStatement("INSERT INTO temporary_results VALUES (?)");
+//
+//            for (int i=0;i<allPossibleValues.size();i++){
+//                ps.setString(1, allPossibleValues.get(i));
+//                System.out.println(i);
+//                ps.addBatch();
+//            }
+//            ps.executeBatch();
 
-            for (int i=0;i<allPossibleValues.size();i++){
-                ps.setString(1, allPossibleValues.get(i));
-                System.out.println(i);
-                ps.addBatch();
+            File csvFile = new File("file.csv");
+            try (PrintWriter csvWriter = new PrintWriter(new FileWriter("file.csv"));){
+                for(String item : allPossibleValues){
+                    csvWriter.println(item);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            ps.executeBatch();
+            ps = con.prepareStatement("COPY temporary_results FROM '/Users/daniel/IdeaProjects/Wordgame_2.0/file.csv' DELIMITER ' ' ;");
+            ps.executeUpdate();
 
             ps = con.prepareStatement("drop table if exists wn_pro_mysql.results;"+
                     "create table wn_pro_mysql.results as " +
