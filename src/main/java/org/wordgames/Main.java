bf5180845e4381d58fd4b8e2cb6072312c003f41
daniel <golikov.danya@gmail.com>
2020-03-07 23:01:41 +0200

    exporter

--- src/main/java/org/wordgames/Main.java
+++ src/main/java/org/wordgames/Main.java
@@ -10,9 +10,8 @@ import static org.wordgames.Permutation.*;
 public class Main {
 
     public static void main(String[] args) {
-        getAllPossibleValues("object",3);
-
         long start = System.currentTimeMillis();
+        getAllPossibleValues("liberation",5);
         PostgresSQLWordChecker.findAllWordsFromArray();
         long finish = System.currentTimeMillis();
         double timeConsumedMillis = finish - start;
