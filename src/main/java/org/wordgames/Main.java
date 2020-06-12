3263e2a8a3377599727b73c2add908d7cac829bf
daniel <golikov.danya@gmail.com>
2020-06-12 14:13:23 +0300

    New revision

--- src/main/java/org/wordgames/Main.java
+++ src/main/java/org/wordgames/Main.java
@@ -1,24 +1,11 @@
 package org.wordgames;
 
-import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
-
-import static org.wordgames.Permutation.*;
-
 
 public class Main {
-
-    public static void main(String[] args) {
-        getAllPossibleValues("object",3);
-
-        long start = System.currentTimeMillis();
-        PostgresSQLWordChecker.findAllWordsFromArray();
-        long finish = System.currentTimeMillis();
-        double timeConsumedMillis = finish - start;
-        System.out.println(timeConsumedMillis/1000);
-
-        Exporter ex = new Exporter();
-        ex.exportIntoCSV();
+    public static void main(String[] args) throws SQLException {
+        WordGameConnector.playWordGame("apple",4);
+        Exporter exporter=new Exporter();
+        exporter.print();
     }
 }
